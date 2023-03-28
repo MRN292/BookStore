6601ef59a03ece51887e02f74034d232acbaa23c
MRN292 <itsmehranmohebi@gmail.com>
2023-03-29 02:02:22 +0330

    second edit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,40 +1,3 @@
-{{-- <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-
-<body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100">
-
-        @include('layouts.navigation')
-
-        <!-- Page Heading -->
-        
-        @yield('header')
-
-        <!-- Page Content -->
-
-        @yield('content')
-
-     
-    </div>
-</body>
-
-</html> --}}
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -55,32 +18,48 @@
 
     <div id="mySidebar" class="sidebar">
         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a href="#">Shop</a>
+        @Auth
+            
+            <a href="/">Shop</a>
 
-        <a href="{{ route('profile.edit') }}">Profile</a>
+            <a href="{{ route('profile.edit') }}">Profile</a>
 
-        <a href="{{ route('show.user') }}">Users</a>
+            <a href="{{ route('show.user') }}">Users</a>
 
-        <a href="#">Books</a>
-        <a href="#">Tags and Genre</a>
+            <a href="#">Books</a>
+            <a href="#">Tags and Genre</a>
 
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
-            <button type="submit">Log Out</button>
-        </form>
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <button type="submit">Log Out</button>
+            </form>
+        @else
+            <a href="{{ route('login') }}">Login</a>
+
+
+        @endauth
+
 
     </div>
 
     <div id="main">
-        <button class="openbtn" onclick="openNav()">&#9776; Dashboard</button>
-        @if (Auth::user()->photo == null)
-        <img class="user-img" src="img/user.png" alt="">
-        @else
-            <img class="user-img" src="uploads/{{ Auth::user()->photo }}" alt="">
-        @endif
-        <span class="user-name">@Auth{{ Auth::user()->name }}@endauth
-        </span>
+        <button class="openbtn" onclick="openNav()">&#9776;</button>
+        @auth
 
+
+            @if (Auth::user()->photo == null)
+            @else
+                <img class="user-img" src="uploads/{{ Auth::user()->photo }}" alt="">
+                <span class="user-name">{{ Auth::user()->name }}</span>
+                <a class="cart-img" href=""><img src="img/cart.png" alt=""></a>
+            @endif
+        @else
+            <img class="user-img" src="img/user.png" alt="">
+            <a href="{{route('login')}}">Login</a>
+        @endauth
+        
+        
+        
     </div>
 
 
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('shop');
 });
 
 
@@ -37,9 +37,9 @@ Route::post('/upload/photo', [UserController::class, 'photo'])->name('upload.pho
 
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
